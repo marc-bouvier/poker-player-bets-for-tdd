e7785376ac7a23adcd1ff7c05d84dac07c1d7688
CodeItQuick <evanontario@hotmail.com>
2024-09-19 13:57:20 -0300

    adjustments

--- src/main/java/org/leanpoker/player/BluffingConstants.java
+++ src/main/java/org/leanpoker/player/BluffingConstants.java
@@ -1,7 +1,7 @@
 package org.leanpoker.player;
 
 public class BluffingConstants {
-    public static int smallPotsBluff = 90;
+    public static int smallPotsBluff = 70;
     public static int mediumPotsBluff = 85;
     public static int largePotsBluff = 75;
     public static int postflopBluffPercentage = 50;
